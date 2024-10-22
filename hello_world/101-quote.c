da73a973644c384b4e568ab0570f424b72209d2b
estebanmhp <estebanhernandezpe@gmail.com>
2024-10-21 21:06:01 -0500

    Print with write, low level instead of printf puts

---
+++ hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s;
+
+	s = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
+
+	return (1);
+}
