aec35e6cc8596327ca30e2c60ba3724d412da6bd
estebanmhp <estebanhernandezpe@gmail.com>
2024-10-23 12:44:18 -0500

    Using while loop and understading ASCII values

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * putchar - Function to print a char
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
