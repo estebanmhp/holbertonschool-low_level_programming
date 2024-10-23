4950aff22773a4e80f0e53789a47d2325dab5937
estebanmhp <estebanhernandezpe@gmail.com>
2024-10-23 15:11:34 -0500

    Printing number in base 16, using values in ASCII to use values in C

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar('0' + n);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
