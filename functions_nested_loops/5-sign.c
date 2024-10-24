b3ed45aaec0ebf416032b6bff0a30bc110418c26
estebanmhp <estebanhernandezpe@gmail.com>
2024-10-24 17:34:07 -0500

    Function print sign, checking which number is positive, negative or zero

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - function to know if it is an alphabet or not
+ * @n: parameter to keep the value of the return
+ * Return: int
+ */
+int print_sign(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		x = 1;
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		x = 0;
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		x = -1;
+	}
+	return (x);
+}
