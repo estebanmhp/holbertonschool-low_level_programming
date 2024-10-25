80a4ac9b31f9f175c7994cc2a71b5f91e13f30bc
estebanmhp <estebanhernandezpe@gmail.com>
2024-10-24 20:44:42 -0500

    Function times tables, printing a table using while nested loop

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - function to print the 9 times tables
+ *
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int n = 0;
+	int m;
+	int x;
+
+	while (n < 10)
+	{
+		m = 0;
+		while (m < 10)
+		{
+			x = n * m;
+			if (x < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + x);
+			}
+			else if (x >= 10)
+			{
+				_putchar('0' + (x / 10));
+				_putchar('0' + (x % 10));
+			}
+			if (m == 9)
+				break;
+			_putchar(',');
+			_putchar(' ');
+			m++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
