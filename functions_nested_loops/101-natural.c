1c1a34fd5f42f43239c47e3d67aba01cea476ea1
estebanmhp <estebanhernandezpe@gmail.com>
2024-10-26 16:59:01 -0500

    More loops with conditional, using +=

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int result;
+	int n;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			result += n;
+	}
+	printf("%d \n", result);
+
+	return (0);
+}
