5e6b736eb9d10576112ac24cd135453a2e38347e
estebanmhp <estebanhernandezpe@gmail.com>
2024-10-28 11:35:20 -0500

    Function print diagonal, printing \ across the terminal according to n

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to print a diagonal according to n
+ * @n: parameter to keep the value of the return
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int s;
+	int d;
+
+	if (n > 0)
+	{
+		for(d = 1; d <= n; d++)
+		{
+			for (s = 1; s <= d; s++)
+			{
+				if (s != 1)
+					_putchar(' ');
+				if (s == d)
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	else
+		_putchar('\n');
+}
