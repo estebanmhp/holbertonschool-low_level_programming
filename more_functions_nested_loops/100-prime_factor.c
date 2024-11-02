2d41929aadfd86c77f644276ef8e1a1d4c044ebf
estebanmhp <estebanhernandezpe@gmail.com>
2024-11-02 15:25:31 -0500

    Finding the bigges prime factor

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	long int n = 612852475143;
+	long int fp;
+	for (fp = 2; fp <= n; fp++){
+		if(n % fp == 0)
+		{
+			n /= fp;
+			fp--;
+		}
+	}
+	printf("%ld\n", fp);
+	return (0);
+}
