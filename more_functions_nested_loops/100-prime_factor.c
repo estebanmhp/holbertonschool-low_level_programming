d5952893fd60a606a85731c8d89fb1fcaf6e6026
estebanmhp <estebanhernandezpe@gmail.com>
2024-11-02 15:27:28 -0500

    Checking betty style

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - Entry point
+ *
+ * Return: Nothing
+ */
+int main(void)
+{
 	long int n = 612852475143;
 	long int fp;
-	for (fp = 2; fp <= n; fp++){
-		if(n % fp == 0)
+
+	for (fp = 2; fp <= n; fp++)
+	{
+		if (n % fp == 0)
 		{
 			n /= fp;
 			fp--;
