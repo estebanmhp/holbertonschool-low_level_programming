163a751bf07069362fc63394e2446e6369c6676d
estebanmhp <estebanhernandezpe@gmail.com>
2024-11-06 12:56:24 -0500

    Encoding a string into leet speak (1337)

---
+++ C-06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * leet - encodes a string into leetspeak (1337)
+ * @s: string to be modified, declarated in the function, not in the header
+ * Return: modified string
+ */
+char *leet(char *s)
+{
+	int i = 0;
+
+	char n[] = "4433007721";
+	char l[] = "aAeEoOtTlL";
+
+
+	while (s[i] != '\0')
+	{
+		int c = 0;
+
+		while (n[c] != '\0' && l[c] != '\0')
+		{
+			if (s[i] == l[c])
+				s[i] = n[c];
+			c++;
+		}
+		i++;
+	}
+
+	return (s);
+}
