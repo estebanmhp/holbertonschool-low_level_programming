10c56245d78a01c14cfa5eda2370d197b18b9210
estebanmhp <estebanhernandezpe@gmail.com>
2024-11-23 16:20:16 -0500

    Printing the name of the programm as an argument, using argc argv

---
+++ C-0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point, prints file name of program
+ * @argc: amount of the arguments
+ * @argv: array of strings containing the arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
