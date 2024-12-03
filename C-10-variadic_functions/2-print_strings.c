260a9cc33e6745313a0f36069babfe4f660ca931
estebanmhp <estebanhernandezpe@gmail.com>
2024-12-03 13:59:48 -0500

    Function to print all strings passed as parameters to a variadic function

---
+++ C-10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints strings passed as args, follwed by newline
+ * @separator: string to be pritned in between arg strings
+ * @n: number of arg strings
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list word;
+
+	va_start(word, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(word, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+}
